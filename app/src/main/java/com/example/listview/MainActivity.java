1596c2dd8064e65c6be0e2c3687372d7e42dc914
Bsef19m519 <bsef19m519@pucit.edu.pk>
2022-06-06 13:37:06 +0500

    Delete record

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -33,13 +35,36 @@ public class MainActivity extends AppCompatActivity {
         arrayList = new ArrayList<String>();
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrayList);
         listView.setAdapter(arrayAdapter);
+        //For sending data on MainActivity2
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+//                intent.putExtra("position",i);
+//                intent.putExtra("value",arrayList.get(i));
+//                startActivity(intent);
+//            }
+//        });
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
-                intent.putExtra("position",i);
-                intent.putExtra("value",arrayList.get(i));
-                startActivity(intent);
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Delete Record")
+                        .setMessage("Do you want to delete?")
+                        .setIcon(R.drawable.ic_launcher_background)
+                        .setCancelable(false)
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                arrayList.remove(i);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
             }
         });
     }
